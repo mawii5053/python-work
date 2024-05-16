aee37afcce64fe84d18a6d8f62b233717b75b2ac
Ifeanyi Agwulonu <ifeanyiagwulonu@gmail.com>
2024-05-16 16:13:12 +0100

    animals

---
+++ chap-ter3_exp/chap-ter4/loooop4.py
@@ -0,0 +1,26 @@
+pharmacists = ['dimoji','mawii','ukiwe','sydney','okolue']
+for pharmacist in pharmacists:
+ print(pharmacist)
+
+ pharmacists = ['dimoji','mawii','ukiwe','sydney','okolue']
+ for pharmacist in pharmacists:
+    print(f"{pharmacist.title()}, i hope you remember the last time you ate fish ?")
+    print(f"i cant wait to take you to a better location where different species of fishes are sampled , {pharmacist.title()}.\n")
+print("thank you everyone")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick")
+    print(f"i cant wait to see your next trick, {magician.title()}.\n")
+
+stations = ['mobil','ooando','jessie','imola']
+for station in stations:
+    print(f"{station.title()} is one of the best filling_station in port harcourt.")
+    print(f"despite the increase in corruption in nigeria, {station.title()}, still makes their volume standard.\n")
+    print('thank you'.title())
+
+    animals = ['frog', 'cat', 'dog', 'rabbit']
+    for animal in animals:
+        print(f"a {animal.title()} is a good pet")
+        print(f"these animals are hamless")
+        print('thank you')
