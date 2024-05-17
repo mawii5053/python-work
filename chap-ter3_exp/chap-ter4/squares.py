e8c13e29610af1b11f381cf2a3ff7e01677bc5bc
Ifeanyi Agwulonu <ifeanyiagwulonu@gmail.com>
2024-05-17 15:04:56 +0100

    squares

---
+++ chap-ter3_exp/chap-ter4/squares.py
@@ -0,0 +1,53 @@
+squares = []
+for value in range(1, 11):
+    square = value **2
+    squares.append(square)
+    print(squares)
+
+squares = []
+for value in range(1, 11):
+    squares.append(value**2)
+    print(squares)
+
+squares = [value **2 for value in range(1,11)]
+print(squares)
+
+for value in range(1,21):
+    print(value)
+
+digits = [1,2,3,4,5,6,7,]
+min(digits)
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+numbers = list(range(1, 1000001))
+print(numbers)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+odd_numbers = list(range(1, 21, 2))
+print(odd_numbers)
+for number in odd_numbers :
+    print(number)
+
+multiples_3 = list(range(3, 31, 3))
+print(multiples_3)
+for num in multiples_3 :
+    print(num)
+
+cubes = []
+for value in range(1,11):
+    cube = value **3
+    cubes.append(cube)
+    print(cubes)
+
+cubes = []
+for value in range(1,11):
+    cubes.append(value **3)
+    print(cubes)
+
+cubes = [value **3 for value in range(1,11)]
+print(cubes)
+
