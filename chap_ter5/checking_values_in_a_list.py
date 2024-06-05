65a4dbc907855950e88f3451a6e9fa39f5feda9d
Ifeanyi Agwulonu <ifeanyiagwulonu@gmail.com>
2024-06-05 21:05:45 +0100

    rose

--- chap_ter5/checking_values_in_a_list.py
+++ chap_ter5/checking_values_in_a_list.py
@@ -1,7 +1,20 @@
 requested_foods = ['rice','beans','fish']
-'rice' in requested_foods
-print(requested_foods)
+if 'moi' in requested_foods:
+    print("requested_foods")
+if "rice" in requested_foods:
+    print(requested_foods)
 
-requested_foods = ['rice','beans','fish']
-'fufu' in requested_foods
-print(requested_foods)
+
+print("\nchecking for value not in the list")
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you can")
+
+drug = "nurofen"
+print("is drug == 'nurofen'? i predict true.")
+print(drug == 'nurofen')
+
+print("\nis drug == 'paracetamol' ? i predict false.")
+print(drug == 'paracetamol')
