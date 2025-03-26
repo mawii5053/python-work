4fd9ac8e0f7a34e6d925ea328854ec68712c854b
Ifeanyi Agwulonu <ifeanyiagwulonu@gmail.com>
2025-03-26 18:43:15 +0100

    zuriel

--- chapter_6/Mama_Ejima.py
+++ chapter_6/Mama_Ejima.py
@@ -2,3 +2,11 @@ print('Rose_age')
 print(2+5)  
 Rose_age = 24+8
 print(Rose_age)
+name='rosemary'
+print(name.title())
+print(name.replace('r','l'))
+print(name.replace('r','l',1))
+name7=input('what is your name? :')
+name7= name7.strip().title()
+greeting=f"Good morning,{name7},You are welcome to class!"
+print(greeting)
